0110c53eac32cbb7250359dc20008c4ec120c859
Adnan Shaikh <67497014+alphakray@users.noreply.github.com>
2021-03-08 20:51:39 +0530

    Add files via upload
    A guessing game with random number generation

---
+++ guess.py
@@ -0,0 +1,18 @@
+import random
+win_number=random.randint(1,100)
+guess=1
+user_number=int(input("Enter a number between 1 to 100 : "))
+game_over=False
+while not game_over:
+    if user_number==win_number:
+        print(f"you WIN!!!, attemps taken {guess}")
+        game_over=True
+    else  :
+        if user_number > win_number:
+            print("Too high")
+            guess+=1
+            user_number=int(input("guess again :"))
+        else:
+            print("Too low")
+            guess+=1
+            user_number=int(input("guess again :"))
